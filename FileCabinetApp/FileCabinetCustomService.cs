8c6ee734ad4b09f48b53d77099df4be334a07930
Darya Kunickaya <darya.kunickaya@gmail.com>
2020-02-13 20:44:58 +0300

    Introduce template method for parameter validation in FileCabinetService.

---
+++ FileCabinetApp/FileCabinetCustomService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp
+{
+    /// <summary>
+    /// For custom validation.
+    /// </summary>
+    public class FileCabinetCustomService
+    {
+
+    }
+}
